b1df627b23ac5d751239b65726c0a0e6c81f82a0
Fatih Aslan <fatihaslan0638@gmail.com>
2026-09-18 01:34:42 +0300

    Staj sonu arayüz ve backend son güncellemeleri

--- TaskFlow.Domain/obj/Debug/net9.0/TaskFlow.Domain.AssemblyInfo.cs
+++ TaskFlow.Domain/obj/Debug/net9.0/TaskFlow.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6764ac7e073b277266816a58b930b92cb42ebb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
